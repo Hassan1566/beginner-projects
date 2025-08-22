0377c819d92e8f1515d030f6a4dc542c511740fe
Hassan1566 <sh456700992sj@gmail.com>
2025-08-22 12:08:23 +0500

    Fix input handling and improve tie message in game logic

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,7 +4,7 @@ Implementation of rock, paper, scissors by Kylie Ying
 YouTube Kylie Ying: https://www.youtube.com/ycubed 
 Twitch KylieYing: https://www.twitch.tv/kylieying 
 Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
+Instagram @kylieyying: https://www.instagram.com/kylieying/ 
 Website: https://www.kylieying.com
 Github: https://www.github.com/kying18 
 Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
@@ -13,23 +13,34 @@ Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5
 import random
 
 def play():
-    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n").lower()
+    if user not in ('r', 'p', 's'):
+        print("Invalid choice. Please enter 'r', 'p' or 's'.")
+        return play_again()
+
     computer = random.choice(['r', 'p', 's'])
 
     if user == computer:
-        return 'It\'s a tie', play_again()
+        print("It's a tie")
+        return play_again()
 
     # r > s, s > p, p > r
     if is_win(user, computer):
-        return 'You won!', play_again()
-    return 'You lost!', play_again()
+        print('You won!')
+        return play_again()
+
+    print('You lost!')
+    return play_again()
 
 def play_again():
-    choose = input("Do you want to play again? (y/n)\n")
+    choose = input("Do you want to play again? (y/n)\n").lower()
     if choose == 'y':
         return play()
-    else:
+    elif choose == 'n':
         return 'Thanks for playing!'
+    else:
+        print("Please enter 'y' or 'n'.")
+        return play_again()
 
 def is_win(player, opponent):
     # return true if player wins
